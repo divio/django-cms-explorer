2a8e57bedd6471b1dad574c179dc2ad39aa9651a
Angelo Dini <angelo.dini@divio.ch>
2016-01-18 15:45:01 +0100

    update boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,97 @@
-/*!
- * @author:    Divio AG
- * @copyright: http://www.divio.ch
+/*
+ * Copyright (c) 2013, Divio AG
+ * Licensed under BSD
+ * http://github.com/aldryn/aldryn-boilerplate-bootstrap3
  */
 
 'use strict';
 
 // #############################################################################
-// #IMPORTS#
-var autoprefixer = require('gulp-autoprefixer');
+// IMPORTS
+var argv = require('minimist')(process.argv.slice(2));
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var karma = require('karma').server;
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var minifyCss = require('gulp-minify-css');
-var protractor = require('gulp-protractor').protractor;
-var webdriverUpdate = require('gulp-protractor').webdriver_update;
 
 // #############################################################################
-// #SETTINGS#
+// SETTINGS
 var PROJECT_ROOT = __dirname;
 var PROJECT_PATH = {
-    'sass': PROJECT_ROOT + '/private/sass',
-    'css': PROJECT_ROOT + '/static/css',
-    'tests': PROJECT_ROOT + '/tests'
+    bower: PROJECT_ROOT + '/static/vendor',
+    css: PROJECT_ROOT + '/static/css',
+    docs: PROJECT_ROOT + '/static/docs',
+    fonts:  PROJECT_ROOT + '/static/fonts',
+    html: PROJECT_ROOT + '/templates',
+    images: PROJECT_ROOT + '/static/img',
+    icons: PROJECT_ROOT + '/private/icons',
+    js: PROJECT_ROOT + '/static/js',
+    sass: PROJECT_ROOT + '/private/sass',
+    tests: PROJECT_ROOT + '/tests'
 };
 
 var PROJECT_PATTERNS = {
-    'sass': [
+    images: [
+        PROJECT_PATH.images + '/**/*',
+        // exclude from preprocessing
+        '!' + PROJECT_PATH.images + '/dummy/*/**'
+    ],
+    js: [
+        'gulpfile.js',
+        PROJECT_PATH.js + '/**/*.js',
+        PROJECT_PATH.tests + '/**/*.js',
+        // exclude from linting
+        '!' + PROJECT_PATH.js + '/*.min.js',
+        '!' + PROJECT_PATH.js + '/**/*.min.js',
+        '!' + PROJECT_PATH.tests + '/coverage/**/*'
+    ],
+    sass: [
         PROJECT_PATH.sass + '/**/*.{scss,sass}'
     ]
 };
 
-// #############################################################################
-// #TASKS#
-gulp.task('sass', function () {
-    gulp.src(PROJECT_PATTERNS.sass)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .on('error', function (error) {
-            gutil.log(gutil.colors.red(
-                'Error (' + error.plugin + '): ' + error.messageFormatted)
-            );
-        })
-        .pipe(autoprefixer({
-            // browsers are coming from browserslist file
-            cascade: false
-        }))
-        .pipe(minifyCss({
-            // fixes font imports from google fonts
-            rebase: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(PROJECT_PATH.css));
-});
+var PORT = parseInt(process.env.PORT, 10) || 8000;
+var DEBUG = argv.debug;
 
-// #######################################
-// #TESTS#
-gulp.task('tests', ['tests:unit', 'tests:integration']);
-gulp.task('tests:unit', function (done) {
-    // run javascript tests
-    karma.start({
-        configFile: PROJECT_PATH.tests + '/karma.conf.js',
-        singleRun: true
-    }, done);
-});
-
-gulp.task('tests:webdriver', webdriverUpdate);
-gulp.task('tests:integration', ['tests:webdriver'], function () {
-    return gulp.src([PROJECT_PATH.tests + '/integration/specs/*.js'])
-        .pipe(protractor({
-            configFile: PROJECT_PATH.tests + '/protractor.conf.js',
-            args: []
-        }))
-        .on('error', function (error) {
-            gutil.log(gutil.colors.red(
-                'Error (' + error.plugin + '): ' + error.message)
-            );
-        });
-});
-
-gulp.task('tests:watch', function () {
-    // run javascript tests
-    karma.start({
-        configFile: PROJECT_PATH.tests + '/karma.conf.js'
+function task(id) {
+    return require('./tools/tasks/' + id)(gulp, {
+        PROJECT_ROOT: PROJECT_ROOT,
+        PROJECT_PATH: PROJECT_PATH,
+        PROJECT_PATTERNS: PROJECT_PATTERNS,
+        DEBUG: DEBUG,
+        PORT: PORT
     });
-});
+}
 
-// #############################################################################
-// #COMMANDS#
+gulp.task('bower', task('bower'));
+gulp.task('lint:javascript', task('lint/javascript'));
+gulp.task('lint', ['lint:javascript']);
+gulp.task('sass', task('sass'));
+gulp.task('build', ['sass']);
+
+/**
+ * GULP_MODE === 'production' means we have a limited
+ * subset of tasks, namely sass, bower and lint to
+ * speed up the deployment / installation process.
+ */
+if (process.env.GULP_MODE !== 'production') {
+    gulp.task('images', task('images'));
+    gulp.task('docs', task('docs'));
+    gulp.task('preprocess', ['sass', 'images', 'docs']);
+    gulp.task('icons', task('icons'));
+
+    gulp.task('browser', task('browser'));
+
+    gulp.task('tests:lint', ['lint:javascript']);
+    gulp.task('tests:unit', task('tests/unit'));
+    gulp.task('tests:watch', ['tests:lint'], task('tests/watch'));
+    gulp.task('tests', ['tests:unit', 'tests:integration', 'tests:lint']);
+
+    var webdriverUpdate = require('gulp-protractor').webdriver_update;
+    gulp.task('tests:webdriver', webdriverUpdate);
+    gulp.task('tests:integration', ['tests:webdriver'], task('tests/integration'));
+}
+
 gulp.task('watch', function () {
     gulp.watch(PROJECT_PATTERNS.sass, ['sass']);
+    gulp.watch(PROJECT_PATTERNS.js, ['lint']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['bower', 'sass', 'lint', 'watch']);
