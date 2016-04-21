d611d7b3cfa27ca251c9dd78193a4f98701aa351
Eleanor Berger <eleanor.berger@divio.com>
2016-04-21 09:58:54 +0200

    Test improvement (by @vxsx)

--- tests/integration/specs/spec.dummy.js
+++ tests/integration/specs/spec.dummy.js
@@ -15,8 +15,6 @@ describe('Django CMS website', function () {
     it('should have a title', function () {
         browser.get(dummyPage.site);
 
-        expect(
-            browser.getTitle().toLowerCase().replace('-', ' '))
-            .toContain('django cms');
+        expect(browser.getTitle()).toMatch(/django.cms/i);
     });
 });
