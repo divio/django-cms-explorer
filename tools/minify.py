76b02d46b2c17d8f1e79f47a3c5a30662de8fc65
Angelo Dini <angelo.dini@divio.ch>
2016-06-29 10:09:20 +0200

    added minify file

---
+++ tools/minify.py
@@ -0,0 +1,30 @@
+standardsite-18 (git:develop) ▸ cat tools/minify.py
+"""
+Minify collected javascript files
+
+This would ideally be moved into either the frontend building pipeline (gulp
+et al.) or integrated into the building process (collectstatic or another
+management command).
+"""
+import os
+import gzip
+
+import rjsmin
+
+
+def minify(staticfiles_path):
+    for root, dirs, files in os.walk(staticfiles_path):
+        for file in files:
+            if not file.endswith('.js'):
+                continue
+            file = os.path.join(root, file)
+            with open(file, 'r+') as fh:
+                minified = rjsmin.jsmin(fh.read())
+                fh.truncate(0)
+                fh.write(minified)
+            if os.path.exists(file + '.gz'):
+                with gzip.open(file + '.gz', 'w') as fh:
+                    fh.write(minified)
+
+
+minify('/app/static_collected')
